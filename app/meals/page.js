eb66ee7062e38bc5e195f3fe4d9f82e9567b7ad2
Samy Nagy <sami.youssef29@yahoo.com>
2024-12-01 16:30:18 +0000

    feat: load meals from sqlite

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,7 +1,29 @@
-export default function MealsPage() {
+import Link from "next/link";
+
+import MealsGrid from "@/components/meals/meals-grid";
+import classes from "./page.module.css";
+import { getMeals } from "../../lib/meals";
+
+export default async function MealsPage() {
+  const meals = await getMeals();
+
   return (
-    <main>
-      <h1>Meals Page</h1>
-    </main>
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, created{" "}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>
+          Chooses your favorite racipe and cook it yourself, It is easy and fun!
+        </p>
+        <p className={classes.cta}>
+          <Link href={"/meals/share"}>Share your favourite recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={meals} />
+      </main>
+    </>
   );
 }
