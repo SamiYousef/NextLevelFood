233cccaf44792f6632a8516dee1e7855e2fc3adf
Samy Nagy <sami.youssef29@yahoo.com>
2024-12-01 14:51:19 +0000

    feat: community page

--- components/mainHeader/main-header.js
+++ components/mainHeader/main-header.js
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import Image from "next/image";
 
+import NavLink from "./nav-link";
 import MainHeaderBackground from "./main-header-background";
 import classes from "./main-header.module.css";
 import LogoIcon from "@/assets/logo.png";
@@ -18,10 +19,10 @@ export default function MainHeader() {
         <nav className={classes.nav}>
           <ul>
             <li>
-              <Link href={"/meals"}>Brows Meals</Link>
+              <NavLink href={"/meals"}>Brows Meals</NavLink>
             </li>
             <li>
-              <Link href={"/community"}>Foodies Community</Link>
+              <NavLink href={"/community"}>Foodies Community</NavLink>
             </li>
           </ul>
         </nav>
